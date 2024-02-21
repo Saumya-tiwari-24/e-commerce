a897e63344894e881a54a4440d675a69007645cd
Saumya Tiwari <98815617+Saumya-tiwari-24@users.noreply.github.com>
2024-02-21 18:02:18 +0530

    Add files via upload

---
+++ client/src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="footer footer-center p-4 border-t text-base-content">
+      <div>
+        <p>Copyright © 2024 - All right reserved by SAUMYA TIWARI</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
